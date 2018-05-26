c27e96861a8112e0517e7a4439c68c2aa45ff66e
adrianwg23 <adrianwg23@hotmail.com>
2018-05-26 03:53:55 -0400

    added order function

--- stdlib-backend/munch/munch/functions/__main__.js
+++ stdlib-backend/munch/munch/functions/__main__.js
@@ -10,8 +10,7 @@ const lib = require('lib')({ token: process.env.STDLIB_TOKEN })
 */
 
 module.exports = async (sender = '', receiver = '', message = '_', createdDatetime = '', context) => {
-  // Try to find a handler for the message, default to __notfound__
-  let handler = message.toLowerCase().trim().replace(/[^a-z0-9_-]/gi, '_') || '_'
+  let handler = message.toLowerCase().trim().replace(/[^a-z0-9_-]/gi, '_') || '_'.split(" ")[0]
   let result
   try {
     result = await lib[`${context.service.identifier}.messaging.${handler}`]({
